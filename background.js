839b2c66f2fffb640a3988fe9b11eeecc99f45e8
akac97 <142359207+akac97@users.noreply.github.com>
2024-04-20 10:10:48 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
 // background.js
 var userAgent = 'Mozilla/5.0 (Linux; Android 13; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36';
 
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-      if (details.requestHeaders[i].name === 'User-Agent') {
-        details.requestHeaders[i].value = userAgent;
-        break;
-      }
+function rewriteUserAgentHeader(e) {
+  e.requestHeaders.forEach(function(header) {
+    if (header.name.toLowerCase() === "user-agent") {
+      header.value = userAgent;
     }
-    return {requestHeaders: details.requestHeaders};
-  },
-  {urls: ['<all_urls>']},
-  ['blocking', 'requestHeaders']
+  });
+  return {requestHeaders: e.requestHeaders};
+}
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  rewriteUserAgentHeader,
+  {urls: ["<all_urls>"]},
+  ["blocking", "requestHeaders"]
 );
